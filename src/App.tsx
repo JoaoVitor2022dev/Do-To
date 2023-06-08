d820585524ac1d3e9e1ed22c3c4907b2890d5fa9
joao vitor <githubconta2021@gmail.com>
2023-06-07 21:59:11 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+// styles
+import './App.css';
+
+// componentes
+import Footer from './components/Footer';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <div className="App">
+        <Header/>
+      <h1>Conteudo...</h1>
+        <Footer/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+        <footer>
+           <p>
+             <span>React + TS todo</span> @2021
+           </p>
+      </footer>
+    </div>
+  )
+}
+
+export default Footer
