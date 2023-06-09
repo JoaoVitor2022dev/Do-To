28f10e44ecb34b41aa85d5932f906cee3707f57f
joao vitor <githubconta2021@gmail.com>
2023-06-09 15:24:58 -0400

    Descriçao: captura os dados do input e tipar eles;

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,9 @@ import Header from './components/Header';
 import TaskForm from './components/TaskForm';
 import TaskList from './components/TaskList';
 
+// interface 
+import { ITask } from './interfaces/Task';
+
 function App() {
   return (
     <div className="App">
--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -1,20 +1,52 @@
 // styles 
 import styles from "./TaskForm.module.css"
 
+// hooks 
+import { useState, ChangeEvent, FormEvent, useEffect } from "react";
+
+// Interface 
+import { ITask } from '../interfaces/Task';
+
 interface Props {
     btnText: string
 } 
 
 const TaskForm = ({ btnText }: Props) => {
+ 
+    const [id, setId] = useState<number>(0); 
+    const [title, setTitle] = useState<String>(""); 
+    const [difficulty, setDifficulty] = useState<number>(0);
+
+
+    const handleSubmit = () => {
+       
+    } 
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+      if (e.target.name === "title") {
+         setTitle(e.target.value) 
+      } else {
+         setDifficulty(parseInt(e.target.value));
+      }
+    };
+
     return (
-        <form className={styles.form}>
+        <form className={styles.form} onSubmit={handleSubmit}>
            <div className={styles.inputContainer}>
               <label htmlFor="title">Titulo:</label> 
-              <input type="text" name="title" placeholder="Título da tarefa" />
+              <input type="text" 
+              name="title" 
+              placeholder="Título da tarefa" 
+              onChange={handleChange}
+              />
            </div>    
            <div className={styles.inputContainer}>
               <label htmlFor="difficulty">Dificuldade:</label> 
-              <input type="text" name="title" placeholder="Dificuldade da tarefa" />
+              <input type="text" 
+              name="title" 
+              placeholder="Dificuldade da tarefa"
+              onChange={handleChange}
+              />
            </div>    
            <input type="submit" value={btnText} />
         </form>
